7c8fade90249548c4c45e5c9a02e7b577ac7dc3f
ablesudden3 <64771032+ablesudden3@users.noreply.github.com>
2021-04-14 23:41:09 +0530

    Update install.sh

--- nginx-controller-deployment-helm/helm_chart/install.sh
+++ nginx-controller-deployment-helm/helm_chart/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-helm install --name nginx-ingress-gclb --namespace nginx-ingress-gclb -f ./values.yaml stable/nginx-ingress --version "1.26.1"
+helm install nginx-ingress -f ./values.yaml stable/nginx-ingress
 
